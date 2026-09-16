a23edad2118284b3c7f8992bee3ff8b1acfdc76a
kumarsahil09100-web <kumarsahil09100@gmail.com>
2026-09-16 21:48:19 +0530

    ✅ BUSSEAT | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/BUSSEAT/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int N,K;
+	    cin>>N>>K;
+	   if (K>=N)
+	   cout <<2*(K-N)<<endl;
+	   else
+	   cout << 0 << endl;
+	   
+}
